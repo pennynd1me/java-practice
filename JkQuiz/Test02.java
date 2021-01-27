f783ef3bb05b32c560f5d43e243eb1c6d4d2b7dc
pennynd1me <4prilkim@gmail.com>
2021-01-27 22:40:16 +0900

    add JkQuiz

---
+++ JkQuiz/Test02.java
@@ -0,0 +1,7 @@
+public class Test02 {
+	public static void main(String[] args) {
+		int num = 456;
+		int result = num-56;
+		System.out.println(result);
+	}
+}
---
+++ JkQuiz/Test04.java
@@ -0,0 +1,8 @@
+public class Test04 {
+	public static void main(String[] args) {
+		int fahrenheit = 100;
+		float celcius = (fahrenheit-32)* ((float)5/9);
+		System.out.println("Fahrenheit:" + fahrenheit);
+		System.out.println("Celcius:" + celcius);
+	}
+}

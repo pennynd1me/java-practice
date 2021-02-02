65687d982cc4f4c34df3f1955452bf99df0ad972
pennynd1me <4prilkim@gmail.com>
2021-02-02 15:23:52 +0900

    add Day8

---
+++ HackerRank/Day8.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Day8 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		Map<String,Integer> phoneBook = new HashMap<String,Integer>();
+		for(int i = 0; i < n; i++){
+			String name = in.next();
+			int phone = in.nextInt();
+			in.nextLine();
+			phoneBook.put(name, phone);
+		}
+		while(in.hasNext()){
+			String s = in.next();
+			if(phoneBook.containsKey(s)) {
+				System.out.println(s + "=" + phoneBook.get(s));
+			}else {
+				System.out.println("Not found");
+			}
+		}
+		in.close();
+	}
+}
